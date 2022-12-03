d17dae6671cbf730d9738b4405ac84c1ecb98b07
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2022-12-03 19:49:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
             InlineKeyboardButton('🤖 Updates', url='https://telegram.me/FHDmovies24x7official')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('🕹 Repo', callback_data='about')
+            InlineKeyboardButton('💎 Get VIP', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -94,7 +94,7 @@ async def start(client, message):
             InlineKeyboardButton('🤖 Updates', url='https://telegram.me/FHDmovies24x7Official')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('🕹 Repo', callback_data='about')
+            InlineKeyboardButton('💎 Get VIP', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
